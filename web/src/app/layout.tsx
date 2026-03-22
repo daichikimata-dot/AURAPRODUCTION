30c1d8aef15d7905412274670781c9f6d25f2c77
AiriAnazawa <aaangsn@gmail.com>
2026-03-22 10:27:33 +0900

    Update layout.tsx
    Add GTM and Search Console

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import type { Metadata } from "next";
 import { Cinzel, Noto_Sans_JP } from "next/font/google";
 import "./globals.css";
@@ -37,10 +38,30 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja">
+<html lang="ja">
+      <head>
+        <meta name="google-site-verification" content="RgpP9k0rA6GynhC6T_CjTaJjYetuzhneaP07r-gd2og" />
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-KBBNVHW5');
+          `}
+        </Script>
+      </head>
       <body
         className={`${cinzel.variable} ${notoSansJP.variable} antialiased font-sans text-stone-800 bg-[#fffafb]`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KBBNVHW5"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {children}
         <StickyCTA />
       </body>
